bbafb324d119a1520452bb0cc735548a42bf15b2
ragewolf254 <ragewolf254@yahoo.com>
2018-01-09 11:50:11 +0800

    Fixed layout issues (Added relative layouts and Linear layouts) Added a functional button for login

--- app/src/main/java/com/example/code/forge/MainActivity.java
+++ app/src/main/java/com/example/code/forge/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.code.forge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("FORGE", "Login has been pressed");
+            }
+        });
     }
 }
