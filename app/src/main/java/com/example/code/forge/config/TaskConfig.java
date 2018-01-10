4d65f0e3252575a82394fc2bbb393dd0de7386fd
Jefferson Rylee <rylee.jeff385@gmail.com>
2018-01-10 21:27:25 +0800

    Added async task and empty task config

---
+++ app/src/main/java/com/example/code/forge/config/TaskConfig.java
@@ -0,0 +1,11 @@
+package com.example.code.forge.config;
+
+/**
+ * Created by User on 01/10.
+ */
+
+public final class TaskConfig {
+    private static final boolean PRODUCTION = false;
+    public static final String BASE_URL = PRODUCTION ? "" : "";
+    public static final String LOGIN_URL = BASE_URL + "";
+}
---
+++ app/src/main/java/com/example/code/forge/utils/SuperTask.java
@@ -0,0 +1,106 @@
+package com.example.code.forge.utils;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.example.code.forge.config.TaskConfig;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by User on 01/10.
+ */
+
+public final class SuperTask extends AsyncTask<Void, Void, String> {
+
+    private final Context context;
+
+    private SuperTask(Context context) {
+        this.context = context;
+    }
+
+    public static void execute(Context context) {
+        new SuperTask(context).execute();
+    }
+
+    public interface TaskListener {
+        void onTaskRespond(String json);
+        ContentValues setRequestValues(ContentValues contentValues);
+    }
+
+    public static String createPostString(Set<Map.Entry<String, Object>> set) throws UnsupportedEncodingException {
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean flag = true;
+
+        for (Map.Entry<String, Object> value : set) {
+            stringBuilder.append( flag ? "" : "&" );
+            flag = false;
+            stringBuilder.append(URLEncoder.encode(value.getKey(), "UTF-8"));
+            stringBuilder.append("=");
+            stringBuilder.append(URLEncoder.encode(value.getValue().toString(), "UTF-8"));
+        }
+
+        return stringBuilder.toString();
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        try {
+            URL url = new URL(TaskConfig.LOGIN_URL);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoInput(true);
+            httpURLConnection.setDoOutput(true);
+
+            OutputStream outputStream = new BufferedOutputStream(httpURLConnection.getOutputStream());
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+
+            String postString = createPostString(((TaskListener)this.context).setRequestValues(new ContentValues()).valueSet());
+            bufferedWriter.write(postString);
+
+            // clear
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            outputStream.close();
+
+            InputStream inputStream = new BufferedInputStream(httpURLConnection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = "";
+
+            while ((line = bufferedReader.readLine()) != null)
+                stringBuilder.append(line);
+
+            // clear
+            bufferedReader.close();
+            inputStream.close();
+
+            httpURLConnection.disconnect();
+
+            return stringBuilder.toString();
+        } catch (Exception ignored) {}
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String json) {
+        super.onPostExecute(json);
+        ((TaskListener)this.context).onTaskRespond(json);
+    }
+}
