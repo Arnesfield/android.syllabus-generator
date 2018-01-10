587c7de43ab9bf57da75c548b5d934414ccd2b9d
ragewolf254 <ragewolf254@yahoo.com>
2018-01-10 11:48:05 +0800

    Removed Lol comment

--- app/src/main/java/com/example/code/forge/MainActivity.java
+++ app/src/main/java/com/example/code/forge/MainActivity.java
@@ -5,19 +5,36 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declare variables
+    TextView username;
+    TextView password;
+    String s_username;
+    String s_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button loginButton = findViewById(R.id.loginButton);
+        username = findViewById(R.id.idNumber);
+        password = findViewById(R.id.userPassword);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("FORGE", "Login has been pressed");
+                s_username = username.getText().toString();
+                s_password = password.getText().toString();
+                if (!(s_username.equals("201510592") && s_password.equals("test"))){
+                    Toast.makeText(getApplicationContext(), "Login failed.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login success.", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
