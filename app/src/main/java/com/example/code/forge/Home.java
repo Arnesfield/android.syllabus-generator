ecd20de02a281ab0103742c075d0583cc47451b9
ragewolf254 <ragewolf254@yahoo.com>
2018-01-23 14:14:20 +0800

    Added home icon

--- app/src/main/java/com/example/code/forge/Home.java
+++ app/src/main/java/com/example/code/forge/Home.java
@@ -25,7 +25,6 @@ public class Home extends AppCompatActivity
     Toolbar m_toolbar = null;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
