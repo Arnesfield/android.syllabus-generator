0fe004e211af98fcdb25be882af179e9c96f3421
ragewolf254 <ragewolf254@yahoo.com>
2018-01-23 13:20:33 +0800

    Updated activities list and deleted old acitivities

--- app/src/main/java/com/example/code/forge/Courses.java
+++ app/src/main/java/com/example/code/forge/Courses.java
@@ -1,6 +1,5 @@
 package com.example.code.forge;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,26 +12,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class Home extends AppCompatActivity
+public class Courses extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-    //Create drawer essentials
-    DrawerLayout drawer;
-    NavigationView navigationView;
-    Toolbar m_toolbar = null;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        //Declare variables
-        String fname = "",mname = "",lname = "",m_id = "",m_username = "",m_password = "",m_status = "",m_type = "";
-
+        setContentView(R.layout.activity_courses);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -45,43 +32,14 @@ public class Home extends AppCompatActivity
             }
         });
 
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        navigationView = findViewById(R.id.nav_view);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        //Navigation View access list
-        View headerView = navigationView.getHeaderView(0);
-
-        //Retrieve values from past activity
-        Bundle preValues = getIntent().getExtras();
-
-        //Basic user actions
-        //Button b_logout = findViewById(R.id.logout); //Button
-        TextView userValues = findViewById(R.id.user_details);
-        TextView m_fullName = headerView.findViewById(R.id.full_name);
-
-        if (preValues != null){
-            fname = preValues.getString("f_name");
-            mname = preValues.getString("m_name");
-            lname = preValues.getString("l_name");
-            m_id = preValues.getString("m_id");
-            m_username = preValues.getString("m_username");
-            m_password = preValues.getString("m_password");
-            m_status = preValues.getString("m_status");
-            m_type = preValues.getString("m_type");
-        }
-
-        userValues.setText("First Name: " + fname + "\nMiddle Name: " + mname
-                + "\nLast Name: " + lname + "\nUser ID: " + m_id + "\nUsername: " + m_username
-                + "\nPassword: " + m_password + "\nUser status:" + m_status + "\nUser type: " + m_type);
-
-        m_fullName.setText(fname.substring(0,1).toUpperCase() + fname.substring(1) + " " + mname.substring(0,1).toUpperCase()+". " + lname.substring(0,1).toUpperCase() + lname.substring(1));
-
     }
 
     @Override
@@ -97,7 +55,7 @@ public class Home extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
+        getMenuInflater().inflate(R.menu.courses, menu);
         return true;
     }
 
@@ -121,22 +79,19 @@ public class Home extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        Intent activityIntent;
-        if (id == R.id.nav_syllabusList) {
-            activityIntent = new Intent(this, Syllabus.class);
-            startActivity(activityIntent);
-            finish();
-        } else if (id == R.id.nav_teachingPlan) {
 
-        } else if (id == R.id.nav_schedules) {
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_insMaterials) {
+        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_courses) {
+        } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_logout) {
-            Toast.makeText(Home.this,"Logout successful",Toast.LENGTH_LONG).show();
-            finish();
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
