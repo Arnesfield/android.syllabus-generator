201d4b75c8d4e58d589b4bf7aadbe73f1b7f851e
ragewolf254 <ragewolf254@yahoo.com>
2018-03-30 19:26:32 +0800

    Finished the taking of course descriptions

--- app/src/main/java/com/ragew/code/forge_v2/CourseFragment.java
+++ app/src/main/java/com/ragew/code/forge_v2/CourseFragment.java
@@ -2,12 +2,15 @@ package com.ragew.code.forge_v2;
 
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.ragew.code.forge_v2.Config.TaskConfig;
 import com.ragew.code.forge_v2.Utils.SuperTask;
@@ -17,13 +20,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CourseFragment extends Fragment implements SuperTask.TaskListener{
+public class CourseFragment extends Fragment{
     private JSONArray courses;
+    Intent intent;
 
     public CourseFragment() {
         // Required empty public constructor
@@ -32,6 +37,14 @@ public class CourseFragment extends Fragment implements SuperTask.TaskListener{
 
     private ArrayList<Course> m_arrayList;
     private ListView courseListView;
+
+    private String id;
+    private String title;
+    private String code;
+    private String description;
+    private String objectives;
+    private String unitsLec;
+    private String unitsLab;
 
 
     @Override
@@ -45,29 +58,37 @@ public class CourseFragment extends Fragment implements SuperTask.TaskListener{
         //Set an adapter for the list view using the arraylist (for population)
         courseListView.setAdapter(new Course.CourseAdapter(getActivity(), android.R.layout.simple_list_item_1, m_arrayList));
         SuperTask.execute(getContext(),"courses", TaskConfig.COURSE_URL);
-
         //Return the view
         return courseView;
+
     }
 
     //Create a method for setting each course
     public void setCourses (JSONArray jsonArray){
         this.courses = jsonArray;
         try {
-            for (int i = 9; i < jsonArray.length(); i++){
+            for (int i = 0; i < jsonArray.length(); i++){
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
 
-                String id = jsonObject.getString("id");
-                String title = jsonObject.getString("title");
-                String code = jsonObject.getString("code");
-                String description = jsonObject.getString("description");
-                String objectives = jsonObject.getString("objectives");
-                String unitsLec = jsonObject.getString("unitsLec");
-                String unitsLab = jsonObject.getString("unitsLab");
+                id = jsonObject.getString("id");
+                title = jsonObject.getString("title");
+                code = jsonObject.getString("code");
+                description = jsonObject.getString("description");
+                objectives = jsonObject.getString("objectives");
+                unitsLec = jsonObject.getString("unitsLec");
+                unitsLab = jsonObject.getString("unitsLab");
 
                 //Send data to constructor
                 m_arrayList.add(new Course(id,title,code,description,objectives,unitsLec,unitsLab));
             }
+
+            courseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Course course = m_arrayList.get(position);
+                    Toast.makeText(getContext(), course.getDescription(),Toast.LENGTH_LONG).show();
+                }
+            });
 
             //Refresh the list view
             doRefreshList();
@@ -80,20 +101,5 @@ public class CourseFragment extends Fragment implements SuperTask.TaskListener{
     //Create a method to refresh the list
     public void doRefreshList(){
         ((Course.CourseAdapter)this.courseListView.getAdapter()).notifyDataSetChanged();
-    }
-
-    @Override
-    public void onTaskRespond(String id, String json) {
-
-    }
-
-    @Override
-    public ContentValues setRequestValues(String id, ContentValues contentValues) {
-        // put values to contentValues
-        //put(key,value)
-        //check controllers for the correct keys
-        //$this ->input ->post(key)
-        contentValues.put("courses","");
-        return contentValues;
     }
 }
