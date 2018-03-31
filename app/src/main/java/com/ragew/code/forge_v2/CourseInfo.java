5531bd9d3bce5baf5b5ce2c9d66184898c0cd1ec
ragewolf254 <ragewolf254@yahoo.com>
2018-03-31 16:58:26 +0800

    Test Push

--- app/src/main/java/com/ragew/code/forge_v2/CourseInfo.java
+++ app/src/main/java/com/ragew/code/forge_v2/CourseInfo.java
@@ -1,8 +1,6 @@
 package com.ragew.code.forge_v2;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -25,7 +23,7 @@ public class CourseInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_info);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         //Get intent values
@@ -79,6 +77,6 @@ public class CourseInfo extends AppCompatActivity {
             descriptionView.setText(descriptions[position]);
             return convertView;
         }
-    }
+    }//
 
 }
