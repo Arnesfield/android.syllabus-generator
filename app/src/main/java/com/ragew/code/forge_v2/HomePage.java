7843272e2bae4fa1c967a2c4d17434f5326f34b6
ragewolf254 <ragewolf254@yahoo.com>
2018-03-31 11:44:23 +0800

    Able to toast the data for every list item

--- app/src/main/java/com/ragew/code/forge_v2/HomePage.java
+++ app/src/main/java/com/ragew/code/forge_v2/HomePage.java
@@ -53,7 +53,7 @@ public class HomePage extends AppCompatActivity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view) { //
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
