600361e21106922e6aa95787e73b74e5e09793c8
Jefferson Rylee <rylee.jeff385@gmail.com>
2018-04-06 02:48:17 +0800

    Added login and logout functionality

--- app/src/main/java/com/code/feutech/forge/LoginActivity.java
+++ app/src/main/java/com/code/feutech/forge/LoginActivity.java
@@ -1,13 +1,133 @@
 package com.code.feutech.forge;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.code.feutech.forge.config.PreferencesList;
+import com.code.feutech.forge.config.TaskConfig;
+import com.code.feutech.forge.items.User;
+import com.code.feutech.forge.utils.TaskCreator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity implements TaskCreator.TaskListener {
+
+    private TextView txtUsername;
+    private TextView txtPassword;
+    private Button btnLogin;
+    private TextInputLayout txtUsernameContainer;
+    private TextInputLayout txtPasswordContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try {
+            this.checkForUser();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         setContentView(R.layout.activity_login);
+
+        txtUsername = findViewById(R.id.login_txt_username);
+        txtPassword = findViewById(R.id.login_txt_password);
+        btnLogin = findViewById(R.id.login_btn_login);
+        txtUsernameContainer = findViewById(R.id.login_txt_username_container);
+        txtPasswordContainer = findViewById(R.id.login_txt_password_container);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doLoading(true);
+                TaskCreator.execute(view.getContext(), "login", TaskConfig.LOGIN_URL);
+            }
+        });
+    }
+
+    private void doLoading(boolean loading) {
+        boolean enable = !loading;
+        txtUsernameContainer.setEnabled(enable);
+        txtPasswordContainer.setEnabled(enable);
+        btnLogin.setEnabled(enable);
+
+        // if loading, remove errors
+        if (loading) {
+            txtUsernameContainer.setError(null);
+            txtPasswordContainer.setError(null);
+        }
+    }
+
+    private void checkForUser() throws JSONException {
+        SharedPreferences sharedPreferences = getSharedPreferences(PreferencesList.PREF_LOGIN, MODE_PRIVATE);
+        int uid = sharedPreferences.getInt(PreferencesList.PREF_USER_ID, -1);
+
+        // if no id set, do nothing
+        if (uid == -1) {
+            return;
+        }
+
+        // get user
+        // String prefUser = sharedPreferences.getString(PREF_USER_JSON, "");
+        // JSONObject jsonUser = new JSONObject(prefUser);
+        // final User user = new User(jsonUser);
+
+        // go to next activity here
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    // task listener
+    @Override
+    public void onTaskRespond(String id, String json) throws Exception {
+        JSONObject response = new JSONObject(json);
+        boolean success = response.getBoolean("success");
+
+        if (!success) {
+            String error = response.getString("error");
+            if (error != null && error.length() > 0) {
+                txtUsernameContainer.setError("");
+                txtPasswordContainer.setError(error);
+                Snackbar.make(btnLogin, error, Snackbar.LENGTH_LONG).show();
+            }
+            throw new Exception("Request failure.");
+        }
+
+        // get user
+        JSONObject jsonUser = response.getJSONObject("user");
+        final User user = new User(jsonUser);
+
+        // save user to sharedpref
+        SharedPreferences sharedPreferences = getSharedPreferences(PreferencesList.PREF_LOGIN, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(PreferencesList.PREF_USER_ID, user.getId());
+        editor.putString(PreferencesList.PREF_USER_JSON, jsonUser.toString());
+        editor.apply();
+
+        this.checkForUser();
+    }
+
+    @Override
+    public void onTaskError(String id, Exception e) {
+        doLoading(false);
+    }
+
+    @Override
+    public ContentValues setRequestValues(String id, ContentValues contentValues) {
+        final String username = txtUsername.getText().toString();
+        final String password = txtPassword.getText().toString();
+        contentValues.put("username", username);
+        contentValues.put("password", password);
+        return contentValues;
     }
 }
---
+++ app/src/main/java/com/code/feutech/forge/config/PreferencesList.java
@@ -0,0 +1,7 @@
+package com.code.feutech.forge.config;
+
+public final class PreferencesList {
+    public static String PREF_LOGIN = "PREF_LOGIN";
+    public static String PREF_USER_ID = "PREF_USER_ID";
+    public static String PREF_USER_JSON = "PREF_USER_JSON";
+}
