0ac954a2e760a9fc7ad6b8ec88a746c404fa69c1
Jefferson Rylee <rylee.jeff385@gmail.com>
2018-07-19 20:32:46 +0800

    Changed logo Updated local url

--- app/src/main/java/com/code/feutech/forge/config/TaskConfig.java
+++ app/src/main/java/com/code/feutech/forge/config/TaskConfig.java
@@ -3,7 +3,7 @@ package com.code.feutech.forge.config;
 public final class TaskConfig {
     public static final String BASE_URL = AppConfig.PRODUCTION
         ? "http://codegenerator.x10host.com/api/"
-        : "http://192.168.1.13/xforge/public/api/";
+        : "http://192.168.1.10/xforge/public/api/";
     public static final String LOGIN_URL = BASE_URL + "login";
     public static final String COURSES_URL = BASE_URL + "courses";
     public static final String ASSIGNS_MY_URL = BASE_URL + "assigns/my";
